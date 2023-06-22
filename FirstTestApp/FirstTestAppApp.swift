4caa0a8521901831d7b28b4220483ac81c972e35
yakupyazar <yakupyazar@gmail.com>
2023-06-22 21:24:33 +0300

    Initial Commit

---
+++ FirstTestApp/FirstTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstTestAppApp.swift
+//  FirstTestApp
+//
+//  Created by Yakup Yazar on 22.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct FirstTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
