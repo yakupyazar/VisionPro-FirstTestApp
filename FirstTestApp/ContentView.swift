739299b02aef2442821bd084f72df072d45dd0f0
yakupyazar <yakupyazar@gmail.com>
2023-06-23 11:57:37 +0300

    VisionPro Application
    Sample for VisionPro

--- FirstTestApp/ContentView.swift
+++ FirstTestApp/ContentView.swift
@@ -10,25 +10,36 @@ import RealityKit
 import RealityKitContent
 
 struct ContentView: View {
+    @State private var selectedObject: String? = nil
+    
     var body: some View {
-        NavigationSplitView {
-            List {
-                Text("Item")
+        NavigationView {
+            List(selection: $selectedObject) {
+                Text("Nesne 1").tag("Nesne 1")
+                Text("Nesne 2").tag("Nesne 2")
+                Text("Nesne 3").tag("Nesne 3")
             }
-            .navigationTitle("Sidebar")
-        } detail: {
-            VStack {
-                Model3D(named: "Scene", bundle: realityKitContentBundle)
-                    .padding(.bottom, 50)
-
-                Text("Hello, world!")
+            .listStyle(SidebarListStyle())
+            .frame(minWidth: 150, idealWidth: 200, maxWidth: 250)
+            
+            if let selectedObject = selectedObject {
+                Text(selectedObject)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .padding()
+                    .navigationBarTitle(Text(selectedObject))
+            } else {
+                Text("Bir nesne seçin")
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .padding()
+                    .navigationBarTitle(Text("Seçim Yapılmadı"))
             }
-            .navigationTitle("Content")
-            .padding()
         }
+        .frame(minWidth: 500, idealWidth: 700, maxWidth: .infinity, minHeight: 400, idealHeight: 500, maxHeight: .infinity)
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
